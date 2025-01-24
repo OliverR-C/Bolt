d748df76e2740841938e459ed3dae970542e2a96
OliverR-C <oliver.roseclementson@gmail.com>
2025-01-24 07:58:43 -0500

    Initial Maze.js Program

---
+++ Maze.js
@@ -0,0 +1,80 @@
+//Team Members: Zachary Ward, Oliver Rose-Clemontine
+//Date: 1.24.2025
+//Program: Maze
+//Hardware: Bolt #SB - 52F9
+
+//Begin Program
+async function startProgram() {
+
+
+//Start & Say start 
+//Bolt will say start over LEDs and say Start
+//Driver: Oliver Rose-Clemontine
+//Nav: Zachary Ward
+
+
+
+
+
+//Green Checkpoint
+//LED's turn Green
+//Driver: Zachary Ward
+//Nav: Oliver Rose-Clemontine
+
+
+
+
+//Sound #1 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Oliver Rose-Clemontine
+//Nav: Zachary Ward
+
+
+
+
+//Red Checkpoint
+//LED's turn Red
+//Driver: Zachary Ward
+//Nav: Oliver Rose-Clemontine
+
+
+//Sound #2 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Oliver Rose-Clemontine
+//Nav: Zachary Ward
+
+
+
+//Purple Checkpoint
+//LED's turn Purple
+//Driver: Zachary Ward
+//Nav: Oliver Rose-Clemontine
+
+
+
+//Sound #3 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Oliver Rose-Clemontine
+//Nav: Zachary Ward
+
+
+
+//Blue Checkpoint
+//LED's turn Blue
+//Driver: Zachary Ward
+//Nav: Oliver Rose-Clemontine
+
+
+
+
+//Finish and Say Finish
+//Bolt will say Finish over LEDs and say Finish out loud
+//Driver: Oliver Rose-Clemontine
+//Nav: Zachary Ward
+
+
+
+
+
+//end code
+  }
